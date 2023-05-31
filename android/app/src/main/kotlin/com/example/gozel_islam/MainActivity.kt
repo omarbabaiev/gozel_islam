d764ead48b697e9c16cb7ac95e642ad0a53c08be
mylif <omarbaba007@mail.ru>
2023-05-31 17:30:29 +0400

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/gozel_islam/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gozel_islam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,34 @@
+ import 'package:flutter/material.dart';
+
+const appBarColor = Colors.red;
+const daylyTopic = """Sual: Peyğəmbərlərin, övliyanın və ya kafirlərin, yaxud heyvanların rəsmlərini çəkmək haramdır?
+
+CAVAB
+
+İstər hörmət etmək, istər təhqir etmək üçün olsun, istər böyük olsun, istər kiçik olsun, istər insan, istər heyvan rəsmi olsun, canlı rəsm çəkmək və heykəl düzəltmək haramdır,  böyük günahdır. Hədisi-şəriflərdə buyurulur ki:
+
+“Canlı rəsmi çəkənə, qiyamətdə “çəkdiyin rəsmə can ver” deyə əzab olunar.” (Müslim)
+
+“Dünyada (zərurətsiz) canlı rəsmi çəkənə, qiyamətdə bu rəsmə can verməsi  söylənərək əzab edilər. Halbuki o, rəsmə can verə bilməz.” (Nəsai)
+
+“Canlı rəsmi çəkən, qiyamətdə ən şiddətli əzab görəcəkdir.” (Buxari)
+
+“Canlı rəsmi çəkərək Allahu təalanın yaratdıqlarına bənzətməyə çalışanlar, qiyamətdə ən şiddətli əzaba uğrayarlar.” (Buxari, Müslim)
+
+“Ya Əli, bütləri, rəsmləri məhv et.” (Müslim)
+
+“Əlinizə keçən canlı rəsmlərini cırıb, pozun.”  (Müslim)
+
+“Rəsm, cünub və it olan otağa rəhmət mələkləri girməz.” (Əbu Davud, Nəsai, İbni Hibban)
+
+“Köpək və heykəl olan otağa rəhmət mələkləri girməz.” (Müslim)
+
+“Cəbrayıl əleyhissəlam “Biz, köpək və rəsm olan otağa girmərik” dedi.” (Buxari, Tabərani)
+
+“Məni aləmlərə rəhmət olaraq göndərən Allahu təala çalğıları, cahiliyyət işlərini və bütləri yox etməyimi əmr etdi.” (Əbu Nuaym)
+
+“(Zərurətsiz) canlı rəsmi çəkənlərin yeri cəhənnəmdir. Axirətdə çəkilən rəsmlərə can veriləcək, o rəsmi çəkənlərə Cəhənnəmdə əzab verəcəkdir.” (Buxari, Müslim)
+
+Rəsulullah, canlı rəsmi çəkənləri lənətlədi. (Buxari)
+
+İsəvi dinində Əflatun fəlsəfəsi və Romalıların bütpərəstliyi qarışaraq, rəsmlərdə, heykəllərdə, ilahlıq sifətləri (Əbədi var olmaq, hər istədiyini edə bilmək, öldürmək, diriltmək, şəfa vermək, qeybləri bilmək xüsusiyyəti) olduğuna inandılar. Belə olduqlarına inanılan rəsmlərə, heykəllərə büt deyilir. Bunlar Allahu təalaya ortaq edilmiş olur. Bunlara hörmət etməyə tapınmaq deyilir. Tapınma işinə də şirk deyilir.""";
